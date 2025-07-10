1f0850d338b3a70dda2b294a1328d223bebc97b2
alfredo601 <61746409+alfredo601@users.noreply.github.com>
2025-07-10 13:22:03 -0300

    Corrige a ordem de inicialização no asgi.py

--- xadrez_torto/asgi.py
+++ xadrez_torto/asgi.py
@@ -1,13 +1,20 @@
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import game.routing
 
+from django.core.asgi import get_asgi_application
+
+# É crucial definir a variável de ambiente ANTES de fazer qualquer outro import do Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xadrez_torto.settings')
 
+# Esta linha inicializa o Django e deixa tudo pronto.
+django_asgi_app = get_asgi_application()
+
+# AGORA que o Django está pronto, podemos importar o resto com segurança.
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import game.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             game.routing.websocket_urlpatterns
